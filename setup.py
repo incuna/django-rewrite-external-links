2e024c041a2b32eed155386dec8a26dc259a30c1
Max Peterson <max@incuna.com>
2012-02-03 14:15:29 +0000

    Fixed setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 from rewrite_external_links import get_version
 setup(
     name = "django-rewrite-external-links",
     packages = ["rewrite_external_links", ],
     include_package_data=True,
-    version = "0.1",
+    version = get_version(),
     description = "Rewrite all external links to go via a message page.",
     author = "Incuna Ltd",
     author_email = "admin@incuna.com",
