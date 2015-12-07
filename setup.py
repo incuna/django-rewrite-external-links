6ae70c88d120378a606f87bddbf58d77459547df
Kevin Etienne <kevin@incuna.com>
2015-12-07 17:32:40 +0000

    Add test suite

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup
+
 from rewrite_external_links import get_version
+
+
+description = (
+    'Rewrite all external (off-site) links to go via a message page, '
+    + 'using a middleware class.'
+)
+
 setup(
-    name = "django-rewrite-external-links",
-    packages = ["rewrite_external_links", ],
+    name="django-rewrite-external-links",
+    packages=["rewrite_external_links"],
     include_package_data=True,
-    version = get_version(),
-    description = "Rewrite all external (off-site) links to go via a message page, using a middleware class.",
-    author = "Incuna Ltd",
-    author_email = "admin@incuna.com",
-    url = "https://github.com/incuna/django-rewrite-external-links",
+    version=get_version(),
+    description=description,
+    author="Incuna Ltd",
+    author_email="admin@incuna.com",
+    url="https://github.com/incuna/django-rewrite-external-links",
 )
