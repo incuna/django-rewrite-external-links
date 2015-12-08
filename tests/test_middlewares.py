00f801c9c1416cbc0514ecb141b0cc9214942845
Kevin Etienne <kevin@incuna.com>
2015-12-08 17:05:05 +0000

    Update tests

--- tests/test_middlewares.py
+++ tests/test_middlewares.py
@@ -1,4 +1,5 @@
 from django.http.response import HttpResponse
+from django.template.defaultfilters import urlencode
 from django.test import TestCase
 from mock import MagicMock
 
@@ -18,26 +19,48 @@ class TestRewriteExternalLinksMiddleware(TestCase):
             request=request,
             response=initial_response,
         )
-        self.assertEqual(processed_response, initial_response)
+        self.assertEqual(processed_response.content, '')
 
     def test_other_content_type(self):
         """response doesn't change if `Content-Type` is not `text/html`."""
         request = MagicMock()
         content_type = 'application/thraud+xml'
-        initial_response = HttpResponse(content=b'0x0', content_type=content_type)
+        content = b'<a href="http://example.com"></a>'
+        initial_response = HttpResponse(content=content, content_type=content_type)
         processed_response = self.middleware.process_response(
             request=request,
             response=initial_response,
         )
-        self.assertEqual(processed_response, initial_response)
+        self.assertEqual(processed_response.content, content)
 
     def test_other_request_path_info(self):
         request = MagicMock()
-        request.META['PATH_INFO'] = '/another-url/'
+        request.META = {'PATH_INFO': '/external-link/'}
         content_type = 'text/html'
-        initial_response = HttpResponse(content=b'0x0', content_type=content_type)
+        content = b'<a href="http://example.com"></a>'
+        initial_response = HttpResponse(content=content, content_type=content_type)
         processed_response = self.middleware.process_response(
             request=request,
             response=initial_response,
         )
-        self.assertEqual(processed_response, initial_response)
+        self.assertEqual(processed_response.content, content)
+
+    def test_response_content(self):
+        path = 'test-path'
+        link = 'http://example.com'
+        request = MagicMock()
+        request.META = {'PATH_INFO': '/another-url/'}
+        request.path = path
+        content_type = 'text/html'
+        content = b'<a    href="{}"></a>'.format(link)
+        initial_response = HttpResponse(content=content, content_type=content_type)
+        processed_response = self.middleware.process_response(
+            request=request,
+            response=initial_response,
+        )
+
+        expected = '<a    href="/external-link/?link={}&next={}"></a>'.format(
+            urlencode(link, safe=''),
+            path,
+        )
+        self.assertEqual(processed_response.content, expected)
